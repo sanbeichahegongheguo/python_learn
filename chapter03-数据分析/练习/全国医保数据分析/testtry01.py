eba4f4ca5664cebbd67c510e04716c21c35eb016
time <time@time.com>
2019-03-21 18:05:00 +0800

    增加函数

--- chapter03-数据分析/练习/全国医保数据分析/testtry01.py
+++ chapter03-数据分析/练习/全国医保数据分析/testtry01.py
@@ -1,22 +1,60 @@
-import matplotlib.pyplot as plt
+import xlrd
+import pprint
+from matplotlib import pyplot as plt
 import numpy as np
+import pandas as pd
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
 
-# 生成数据
-x = np.linspace(0, 20, 20)
-y1 = np.random.randint(50, 100, 20)
-y2 = np.random.randint(50, 100, 20)
-y3 = np.random.randint(50, 100, 20)
+def ls(path='*'):
+    import glob
+    return glob.glob(path)
 
-# 堆积柱状图
-plt.barh(x, y1, color='r', label='语文')
-plt.barh(x, y2, left=y1, color='g', label='数学')
-plt.barh(x, y3, left=y1+y2, color='c', label='英语')
 
-# 显示范围
-plt.ylim(-2, 22)
-plt.xlim(0, 300)
+def fraw_peopledata(file, title):
+    path = file
+    data_people = xlrd.open_workbook(path)
+    table = data_people.sheets()[0]
+    age = table.col_values(0, start_rowx=8, end_rowx=28)
+    male = table.col_values(5, 8, 28)
+    female = table.col_values(6, 8, 28)
+    # 男性数量朝左。
+    for i in range(len(male)):
+        male[i] = -male[i]
+    plt.figure(figsize=(20,10))
+    plt.barh(age, female, color='r', label='女性')
+    plt.barh(age, male, color='b', label='男性')
 
-# 添加图例
-plt.legend(loc='lower right')
+    for i in range(len(female)):
+        plt.text(female[i]+0.1, i-0.05, "%.2f%%" % female[i], fontdict={'size': 14, 'color':  'red'})
+        plt.text(male[i]-0.45, i-0.1, "%.2f%% "% abs(male[i]), fontdict={'size': 14, 'color':  'blue'})
 
-plt.show()
+    # 显示范围
+    plt.ylim(-2, 22)
+    plt.xlim(-6, 6)
+
+    plt.xlabel("性别（男/女）", fontsize=20)
+    plt.ylabel("年龄组（岁）", fontsize=20)
+    plt.title("%d年中国人口年龄性别组成" % title, fontsize=25)
+
+    # 添加图例
+    plt.legend(loc='upper right')
+    plt.pause(0.3)
+    plt.show()
+
+years = range(2004, 2014)
+files = ls("./datas/国家统计局/*")
+
+import matplotlib.animation as animation
+ims = []
+n = len(files)
+for j in range(n):
+    for i in years:
+        if (str(i) in files[j]):            
+            fraw_peopledata(files[j], i)
+
+            # ims.append(im)
+#             print(i)
+#             print(files[j])
+# ani = animation.ArtistAnimation(fig, ims, interval=200, repeat_delay=1000)
+# ani.save("test.gif",writer='pillow')
